27e019394b710d9306bf571acd9a045a97c027b6
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-07-25 17:45:05 +0530

    commit 4

--- qrcode.py
+++ qrcode.py
@@ -1,4 +1,8 @@
 import qrcode
+from PIL import Image
 
 img = qrcode.make("https://www.linkedin.com/in/vrushal-more-018a252b2/")
 img.save("vrushal_linkedin.png")
+print("QR code saved as 'vrushal_linkedin.png'")
+img = Image.open("vrushal_linkedin.png")
+img.show()
