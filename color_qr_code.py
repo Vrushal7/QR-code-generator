4fa8443faaf1ebce556a6ea86f3546b349327cb7
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-07-25 20:47:52 +0530

    commit 6

--- color_qr_code.py
+++ color_qr_code.py
@@ -1,1 +1,10 @@
 import qrcode
+from PIL import Image
+import qrcode.constants
+
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box=10,border=4)
+qr.add_data("https://www.linkedin.com/in/vrushal-more-018a252b2/")
+qr.make(fit=True)
+
+img=qr.make_image(fill_color="green",back_color="black")
+img.save("vrushal_linkedin_color.png")
