007468dd9079625b81a2e07f20b2eb14b0c413d2
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-07-25 21:04:52 +0530

    commit new

--- color_qr_code.py
+++ color_qr_code.py
@@ -5,5 +5,5 @@ qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L,
 qr.add_data("https://www.linkedin.com/in/vrushal-more-018a252b2/")
 qr.make(fit=True)
 
-img = qr.make_image(fill='green', back_color='black')
-img.save("vrushal_linkedin_color_new.png")
+img = qr.make_image(fill_color='red', back_color='black')
+img.save("vrushal_linkedin_color_new_1.png")
