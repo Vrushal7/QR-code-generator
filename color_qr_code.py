996f22115b17dd35cd09be70a565c60c1361bbf6
Vrushal more <166034175+Vrushal7@users.noreply.github.com>
2024-07-25 20:56:46 +0530

    commit 8

--- color_qr_code.py
+++ color_qr_code.py
@@ -1,10 +1,9 @@
 import qrcode
 from PIL import Image
-import qrcode.constants
 
-qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box=10,border=4)
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
 qr.add_data("https://www.linkedin.com/in/vrushal-more-018a252b2/")
 qr.make(fit=True)
 
-img=qr.make_image(fill_color="green",back_color="black")
+img = qr.make_image(fill='black', back_color='white')
 img.save("vrushal_linkedin_color.png")
